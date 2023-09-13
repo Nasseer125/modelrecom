68a800f6ae42233f5db6c57a6b57337221b2e2fa
Nas WR <45099982+Nasseer125@users.noreply.github.com>
2023-09-13 11:46:23 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,31 +1,55 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import pandas as pd
+import numpy as np
 import streamlit as st
-from streamlit.logger import get_logger
+import joblib
 
-LOGGER = get_logger(__name__)
+# Chargement des données (contenant les noms des films, les identifiants de films et les genres)
+data = pd.read_csv('df_merge.csv')
+df_ratings = pd.read_csv('df_ratings.csv')
+df_movies = pd.read_csv('df_movies.csv')
 
+def generate_recommendation(model, user_id, ratings_df, movies_df, n_items):
+    # Obtenez une liste de tous les identifiants de films à partir de l'ensemble de données
+    movie_ids = df_ratings["movieId"].unique()
+    # Obtenir une liste de tous les films qui ont été regardés par l'utilisateur.
+    movie_ids_user = df_ratings.loc[df_ratings["userId"] == user_id, "movieId"]
+    # Obtenir une liste de tous les films IDS qui n'ont pas été regardés par l'utilisateur
+    movie_ids_to_pred = np.setdiff1d(movie_ids, movie_ids_user)
+ 
+   # Appliquer une note de 4 à toutes les interactions (uniquement pour correspondre au format de l'ensemble de données Surprise)
+    test_set = [[user_id, movie_id, 4] for movie_id in movie_ids_to_pred]
+ 
+   # Prévoir les évaluations et générer des recommandations
+    predictions = model.test(test_set)
+    pred_ratings = np.array([pred.est for pred in predictions])
+    print("Top {0} recommandations d'éléments pour l'utilisateur {1}:".format(n_items, user_id))
+   # Classer les films les plus populaires en fonction des évaluations prédites
+    index_max = (-pred_ratings).argsort()[:n_items]
+    for i in index_max:
+        movie_id = movie_ids_to_pred[i]
+        print(df_movies[df_movies["movieId"]==movie_id]["title"].values[0], pred_ratings[i])
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+# Charger le modèle hybride
+hybrid_model = joblib.load("svdpp_model.pkl")
 
-    st.write("# Welcome to Streamlit! 👋")
+# Interface utilisateur Streamlit
+st.title("Système de recommandation de films")
 
+# Formulaire de saisie utilisateur
+user_id = st.text_input("Entrez l'ID de l'utilisateur:")
+n_items = st.text_input("Entrez l'ID du film:")
+
+if user_id and movie_id:
+    try:
+        user_id = int(user_id)
+        n_items = int(n_items)
+
+        # Faire une recommandation en utilisant le modèle hybride
+        prediction = generate_recommendation(hybrid_model,user_id,df_ratings,df_movies,n_items)
+
+        st.write(f"La prédiction de la note pour l'utilisateur {user_id} et le film {n_items} est {prediction:.2f}")
+    except ValueError:
+        st.write("Veuillez entrer des valeurs numériques valides.")
     st.sidebar.success("Select a demo above.")
 
     st.markdown(
